e1a1b06622c8bdbcbf7199170d6bcb388f852515
Cyrus Cursetjee <ccursetjee@gmail.com>
2021-01-05 12:40:06 -0500

    pigLatinBest

--- PigLatin.java
+++ PigLatin.java
@@ -35,6 +35,38 @@ public class PigLatin {
 		}
 	}
 
+	//only for lowercase
+	private static boolean isLetter(char c) {
+		return c >= 'a' && c <= 'z';
+	}
+
+	public static String pigLatinBest(String s) {
+		String str = s.toLowerCase();
+		String result;
+		char punctuation = ' ';
+		if (! isLetter(s.charAt(s.length() - 1)) ) {
+			punctuation = s.charAt(s.length() - 1);
+			str = str.substring(0, str.length() - 1);
+		}
+		
+		String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+		String vowels = "aeiou";
+
+		if (! isLetter(str.charAt(0))) {
+			result = str;
+		} else if (contains(digraphs, s.substring(0,2))) {
+			result = str.substring(2, str.length()) + str.substring(0, 2) + "ay";
+		} else if (vowels.indexOf(str.charAt(0)) != -1) {
+			result = str + "hay";
+		} else {
+			result = str.substring(1, str.length()) + str.charAt(0) + "ay";
+		}
+		if (punctuation != ' ') {
+			return result + punctuation;
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(pigLatinSimple("mock"));
 		System.out.println(pigLatinSimple("pie"));
@@ -46,5 +78,14 @@ public class PigLatin {
 		System.out.println(pigLatin("skee"));
 		System.out.println(pigLatin("emu"));
 		System.out.println(pigLatin("grade"));
+
+		System.out.println(pigLatinBest("*emu"));
+		System.out.println(pigLatinBest("4chan"));
+		System.out.println(pigLatinBest("fish!"));
+		System.out.println(pigLatinBest("fish"));
+		System.out.println(pigLatinBest("the."));
+		System.out.println(pigLatinBest("cat!"));
+		System.out.println(pigLatinBest("amazing?"));
+		System.out.println(pigLatinBest("apple%"));
 	}
 }
